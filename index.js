50034440b1b2bbe37721bb961727b0584492353e
Michael Beck <michaelbeck1206@gmail.com>
2025-05-22 10:00:48 +1200

    Earth Textures + Moon

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const h = window.innerHeight
 const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.setSize(w, h)
 document.body.appendChild(renderer.domElement)
-const fov = 76
+const fov = 75
 const aspect = w / h
 const near = 0.1
 const far = 10
@@ -21,35 +21,67 @@ const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
 controls.dampingFactor = 0.05
 
+// Textures //
+
+const loader = new THREE.TextureLoader()
+const earthTexture = loader.load('assets/textures/8k_earth_daymap.jpg')
+
 // Sphere Mesh //
 
-const geo = new THREE.IcosahedronGeometry(1.0, 2)
-const mat = new THREE.MeshStandardMaterial({
-  color: 0xffffff,
-  flatShading: true,
+const earthGeo = new THREE.IcosahedronGeometry(1.0, 16)
+const earthMat = new THREE.MeshStandardMaterial({
+  // color: 0xffffff,
+  // flatShading: true,
+  map: earthTexture,
 })
-const mesh = new THREE.Mesh(geo, mat)
-scene.add(mesh)
+const earthMesh = new THREE.Mesh(earthGeo, earthMat)
+scene.add(earthMesh)
+
+const earthNightTexture = loader.load('assets/textures/8k_earth_nightmap.jpg')
+const earthNightMat = new THREE.MeshStandardMaterial({
+  map: earthNightTexture,
+  blending: THREE.AdditiveBlending,
+})
+const earthNightMesh = new THREE.Mesh(earthGeo, earthNightMat)
+// earthNight
+earthMesh.add(earthNightMesh)
 
 // Wire Mesh encasing the Sphere //
 
-const wireMat = new THREE.MeshBasicMaterial({
-  color: 0x000000,
-  wireframe: true,
+// const wireMat = new THREE.MeshBasicMaterial({
+//   color: 0x000000,
+//   wireframe: true,
+// })
+// const wireMesh = new THREE.Mesh(earthGeo, wireMat)
+// earthMesh.add(wireMesh)
+// wireMesh.scale.setScalar(1.001)
+
+// Second Sphere Mesh //
+
+const moonGeo = new THREE.IcosahedronGeometry(1.0, 16)
+const moonMat = new THREE.MeshStandardMaterial({
+  color: 0x99a3a4,
+  flatShading: true,
 })
-const wireMesh = new THREE.Mesh(geo, wireMat)
-mesh.add(wireMesh)
-wireMesh.scale.setScalar(1.001)
+const moonMesh = new THREE.Mesh(moonGeo, moonMat)
+moonMesh.scale.setScalar(0.24)
+moonMesh.position.x = 5
+earthMesh.add(moonMesh)
 
 // Lighting //
-const hemiLight = new THREE.HemisphereLight(0x0099ff, 0xff8500)
-scene.add(hemiLight)
+const sunLight = new THREE.DirectionalLight(0xffffff)
+sunLight.position.set(-2, 0.5, 1.5)
+scene.add(sunLight)
+// const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff)
+// hemiLight.position.set(2, -0.5, 1.5)
+// scene.add(hemiLight)
 
 // Animation //
 function animate(t = 0) {
   requestAnimationFrame(animate)
-  // mesh.scale.setScalar(Math.cos(t * 0.001) + 1.0)
-  mesh.rotation.y = t * 0.0001
+  // earthMesh.scale.setScalar(Math.cos(t * 0.001) + 1.0)
+  earthMesh.rotation.y = t * 0.00005
+  earthMesh.rotation.z = (-23.4 * Math.PI) / 180
   renderer.render(scene, camera)
   controls.update()
 }
